810458e5e8cd409e44625c89d072f366f159c59d
Rosen Hristov <ros.hristov@gmail.com>
2023-05-31 16:03:01 +0300

    Wrote down what needs to be added to world.cpp

---
+++ Code/world.cpp
@@ -0,0 +1,14 @@
+
+//this file should have these functions:
+
+//showing options on where to go that are randomly decided (an example: "Where do you want to go?").
+//                                                                      1. village
+//                                                                      2. dungeon
+//if you wrote something invalid, it should ask to try again and to show the same prompt again.
+
+//what can happen at these locations (at the village, you can buy stuff; at the dungeon, you fight enemies).
+//the shops can be implemented thanks to the items file having a good base, but the combat will have to wait a little so that i
+//can finish the character stats.
+
+//P.S. at the inventory files, it asks for a class "Item", even though that class isn't set in 'items.h'. i fixed it enough so that
+//there isn't an error, but i don't know what data to place in it, so if you have time, check it out.
